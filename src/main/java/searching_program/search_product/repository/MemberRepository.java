dd67c8f6f1546b8b326b959ce13c726a51f0717d
hwang <hwangsangik@gmail.com>
2024-08-13 23:31:28 +0900

    refactor = "일부 searching 기능 추가"

--- src/main/java/searching_program/search_product/repository/MemberRepository.java
+++ src/main/java/searching_program/search_product/repository/MemberRepository.java
@@ -2,12 +2,19 @@ package searching_program.search_product.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import searching_program.search_product.domain.Member;
+import searching_program.search_product.dto.MemberDto;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameContaining(String username);
+
     List<Member> findByAgeGreaterThan(int age);
-    Optional<Member> findByUsername(String username);}
+
+    boolean existsByUserId(String userId);
+
+    Optional<Member> findByUserId(String userId);
+}
