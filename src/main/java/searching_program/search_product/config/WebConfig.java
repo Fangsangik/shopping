6343137d0c79e50e8f926e4c72e5fc8bef65ea52
hwang <hwangsangik@gmail.com>
2024-08-18 21:10:10 +0900

    feat = "DeleteMapping 부분을 Post로 변경했기에 addViewControllers 필요 없고, 대신 HTML은 Post,DELETE, PUT을 적용하기 위해 HiddenHttpMethodFilter을 적용함 "

--- src/main/java/searching_program/search_product/config/WebConfig.java
+++ src/main/java/searching_program/search_product/config/WebConfig.java
@@ -1,12 +1,20 @@
 package searching_program.search_product.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    //HTML form에서 put delete, patch 등 HTTP 메서드 지원
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 
     private final StringToLocalDatTimeConverter stringToLocalDatTimeConverter;
 
@@ -20,9 +28,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addConverter(stringToLocalDatTimeConverter);
     }
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/members/delete/{id}")
-                .setViewName("forward:/error");
-    }
+    //특정 경로에 대한 Get 요청을 처리 하기 위한 방법
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/members/delete/{id}")
+//                .setViewName("deleteForm");
+//    }
 }
