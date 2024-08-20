6280979f6b27c95ad86d4cd8cca51cb7855170ff
hwang <hwangsangik@gmail.com>
2024-08-20 22:01:24 +0900

    refactor = "deleteByUserIdAndItemId를 deleteByMemberIdAndItemId로 변경"

--- src/main/java/searching_program/search_product/repository/ItemFavoriteRepository.java
+++ src/main/java/searching_program/search_product/repository/ItemFavoriteRepository.java
@@ -7,7 +7,7 @@ import searching_program.search_product.domain.ItemFavorite;
 import java.util.List;
 
 public interface ItemFavoriteRepository extends JpaRepository<ItemFavorite, Long> {
-    void deleteByUserIdAndItemId(Long memberId, Long itemId);
+    void deleteByMemberIdAndItemId(Long memberId, Long itemId);
 
     List<Item> findItemsByMemberId(Long memberId);
 }
