110c74f6df4976459deecb56cd37fbda70e0e774
hwang <hwangsangik@gmail.com>
2024-08-20 22:04:14 +0900

    refactor = "converter 기능 추가, validateMember에서 null값 확인, logout 세션 무효화 이전에 작업 처리 하는 기능 추가"

--- src/main/java/searching_program/search_product/service/LoginService.java
+++ src/main/java/searching_program/search_product/service/LoginService.java
@@ -6,6 +6,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import searching_program.search_product.domain.Member;
+import searching_program.search_product.dto.DtoEntityConverter;
 import searching_program.search_product.dto.MemberDto;
 import searching_program.search_product.repository.MemberRepository;
 
@@ -24,7 +25,8 @@ public class LoginService {
     private final MemberService memberService;
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
-    private Map<Long, Integer> loginAttempts = new HashMap<>();
+    private final DtoEntityConverter converter;
+    private final Map<Long, Integer> loginAttempts = new HashMap<>();
 
     // 비밀번호 해시화 메서드
     private String encodePassword(String rawPassword) {
@@ -39,14 +41,22 @@ public class LoginService {
         // 비밀번호 해시화
         memberDto.setPassword(encodePassword(memberDto.getPassword()));
 
-        Member member = convertToEntity(memberDto);
+        Member member = converter.convertToMemberEntity(memberDto);
         memberRepository.save(member);
         return true;
     }
 
     // 동일 아이디 존재 검사
     private boolean validateMember(MemberDto memberDto) {
-        Optional<Member> findMember = memberRepository.findById(memberDto.getId());
+        if (memberDto.getUserId() == null) {
+            throw new IllegalArgumentException("userId 값이 null이면 안됩니다");
+        }
+
+        if (memberDto.getPassword() == null) {
+            throw new IllegalArgumentException("password 값이 null이면 안됩니다.");
+        }
+
+        Optional<Member> findMember = memberRepository.findByUserId(memberDto.getUserId());
         return findMember.isPresent();
     }
 
@@ -74,7 +84,6 @@ public class LoginService {
     }
 
 
-
     public void createSession(HttpServletRequest request, MemberDto memberDto) {
         HttpSession session = request.getSession();
         session.setMaxInactiveInterval(1800); // 세션 만료 시간
@@ -82,12 +91,11 @@ public class LoginService {
     }
 
     public Optional<MemberDto> loginTryCountLimit(MemberDto memberDto) {
-        Long memberId = memberDto.getId();
+        Long memberId = memberDto.getId(); // PK 사용
         int attempts = loginAttempts.getOrDefault(memberId, 0);
 
         if (attempts >= 5) {
             memberService.lockMember(memberId);
-            log.info("로그인 실패 {}", memberId);
             throw new IllegalArgumentException("계정이 잠겼습니다.");
         }
 
@@ -100,12 +108,10 @@ public class LoginService {
                 return Optional.of(findMember);
             } else {
                 loginAttempts.put(memberId, attempts + 1);
-                log.info("로그인 실패 , 비밀번호 불일치 {}", memberId);
                 return Optional.empty();
             }
         } catch (IllegalArgumentException e) {
             loginAttempts.put(memberId, attempts + 1);
-            log.info("로그인 실패, 존재하지 않음", memberId);
             return Optional.empty();
         }
     }
@@ -113,43 +119,10 @@ public class LoginService {
     public void logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session != null) {
+            //세션 무효화 이전에 작업 처리
+            Long memberId = (Long) session.getAttribute("member");
+            log.info("로그아웃 성공, memberId : {}", memberId);
             session.invalidate();
-            log.info("로그아웃 성공");
         }
-    }
-
-    /*
-     MemberDto를 데이터베이스에 저장하거나 비즈니스 로직에서 사용하기 위해 Member 엔티티로 변환할 때 사용
-     엔티티는 일반적으로 데이터베이스와의 상호작용을 담당하며,
-     비즈니스 로직에 따라 데이터베이스에 저장되거나 업데이트됨.
-     */
-    private Member convertToEntity(MemberDto memberDto) {
-        return Member.builder()
-                .id(memberDto.getId())
-                .userId(memberDto.getUserId())
-                .username(memberDto.getUsername())
-                .age(memberDto.getAge())
-                .address(memberDto.getAddress())
-                .grade(memberDto.getGrade())
-                .paymentMethod(memberDto.getPaymentMethod())
-                .memberStatus(memberDto.getMemberStatus())
-                .password(memberDto.getPassword()) // 이미 해시화된 비밀번호를 사용
-                .build();
-    }
-
-    // Member -> MemberDto 객체로 변환
-    // Member 객체를 클라이언트에게 전달하기 위해 DTO로 변환할 때 사용
-    private MemberDto convertToDto(Member member) {
-        return MemberDto.builder()
-                .id(member.getId())
-                .userId(member.getUserId())
-                .username(member.getUsername())
-                .age(member.getAge())
-                .address(member.getAddress())
-                .grade(member.getGrade())
-                .paymentMethod(member.getPaymentMethod())
-                .memberStatus(member.getMemberStatus())
-                .password(member.getPassword())
-                .build();
     }
 }
