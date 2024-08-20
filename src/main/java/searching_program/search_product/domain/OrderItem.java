721dd31b36e71a24d8e04845a8560180377b8d99
hwang <hwangsangik@gmail.com>
2024-08-20 22:09:42 +0900

    refactor = "Order -> Orders"

--- src/main/java/searching_program/search_product/domain/OrderItem.java
+++ src/main/java/searching_program/search_product/domain/OrderItem.java
@@ -18,12 +18,12 @@ public class OrderItem {
 
     @ManyToOne
     @JoinColumn(name = "order_id")
-    private Order order;
+    private Orders order;
 
     @ManyToOne
     @JoinColumn(name = "item_id")
     private Item item;
 
     private int quantity;
-    private int price; // 총 가격 (수량 * 단가)
+    private int price;
 }
