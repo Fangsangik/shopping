ea79fcfe7d08a18f204b72d470a1d3df309d9998
hwang <hwangsangik@gmail.com>
2024-08-20 22:14:46 +0900

    refactor = itemId 필드값 추가"

--- src/main/java/searching_program/search_product/dto/ShipmentDto.java
+++ src/main/java/searching_program/search_product/dto/ShipmentDto.java
@@ -1,7 +1,6 @@
 package searching_program.search_product.dto;
 
 import lombok.*;
-import searching_program.search_product.domain.Order;
 import searching_program.search_product.type.ShipmentStatus;
 import java.time.LocalDateTime;
 
@@ -13,6 +12,7 @@ import java.time.LocalDateTime;
 public class ShipmentDto {
 
     private Long id;
+    private Long itemId;
     private String curLocation;
     private ShipmentStatus shipmentStatus;
     private LocalDateTime estimatedDeliveryDate;
