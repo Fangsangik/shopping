49411cb31d097e6f5422302ef53cfd0e30b48fed
hwang <hwangsangik@gmail.com>
2024-08-20 22:00:37 +0900

    refactor = "필드값 명명 일치화, category 필드값 추가"

--- src/main/java/searching_program/search_product/dto/ItemDto.java
+++ src/main/java/searching_program/search_product/dto/ItemDto.java
@@ -10,13 +10,15 @@ import lombok.*;
 public class ItemDto {
 
     private Long id;
-
     private String itemName;
+    private Long itemId;
     private String link;
     private String image;
-    private int price;
+    private int itemPrice; // 엔티티의 itemPrice와 일치하도록 변경
     private int lowPrice;
     private int maxPrice;
     private int myPrice;
     private int stock;
+    private String category;
 }
+
