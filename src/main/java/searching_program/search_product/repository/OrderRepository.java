9224e11c97e320d8573b16631a32f027ae3454c2
hwang <hwangsangik@gmail.com>
2024-08-20 22:10:20 +0900

    refactor = "1. findByItemName -> findByItemItemName 2. findByMemberId -> findByMemberUserId"

--- src/main/java/searching_program/search_product/repository/OrderRepository.java
+++ src/main/java/searching_program/search_product/repository/OrderRepository.java
@@ -1,17 +1,16 @@
 package searching_program.search_product.repository;
 
-import org.aspectj.weaver.ast.Or;
+import org.hibernate.criterion.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import searching_program.search_product.domain.Order;
+import searching_program.search_product.domain.Orders;
 
 import java.util.List;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Orders, Long> {
 
-    Page<Order> findByItemName(String itemDto, Pageable pageable);
+    Page<Orders> findByItemItemName(String itemDto, Pageable pageable);
 
-    Page<Order> findByMemberId (Long memberId, Pageable pageable);
-
+    Page<Orders> findByMemberUserId (String userId, Pageable pageable);
 }
