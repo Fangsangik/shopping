8d798fcb1df08910f10f1a47f3f1619a22617a91
hwang <hwangsangik@gmail.com>
2024-08-22 20:10:26 +0900

    feat = "findByItemNames 추가"

--- src/main/java/searching_program/search_product/service/ItemService.java
+++ src/main/java/searching_program/search_product/service/ItemService.java
@@ -10,11 +10,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import searching_program.search_product.domain.Category;
 import searching_program.search_product.domain.Item;
 import searching_program.search_product.domain.ItemFavorite;
 import searching_program.search_product.domain.Member;
 import searching_program.search_product.dto.DtoEntityConverter;
 import searching_program.search_product.dto.ItemDto;
+import searching_program.search_product.repository.CategoryRepository;
 import searching_program.search_product.repository.ItemRepository;
 import searching_program.search_product.repository.MemberRepository;
 import searching_program.search_product.service.notification.NotificationService;
@@ -36,30 +38,43 @@ public class ItemService {
     private final ItemRepository itemRepository;
     private final DtoEntityConverter converter;
     private final NotificationService notificationService;
-    private final MemberRepository memberRepository;
+    private final CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     @Cacheable("itemsByName") //자주 조회되는 데이터를 캐싱 -> DB에 대한 요청을 줄일 수 있음
-    public List<ItemDto> findByItemName(String itemName){
+    public List<ItemDto> findByItemName(String itemName) {
         List<Item> items = itemRepository.findByItemName(itemName);
         return items.stream()
                 .map(converter::convertToItemDto)
                 .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
+    @Cacheable("itemsByNames") //자주 조회되는 데이터를 캐싱 -> DB에 대한 요청을 줄일 수 있음
+    public List<ItemDto> findByItemNames(String itemName1, String itemName2) {
+        List<Item> items = itemRepository
+                .findByItemNameContainingOrItemNameContaining
+                        (itemName1, itemName2);
+        return items.stream()
+                .map(converter::convertToItemDto)
+                .collect(Collectors.toList());
+    }
+
     /**
-     Specification -> JPA에서 동적 쿼리를 작성할 수 있도록 지원하는 인터페이스
-     *동적쿼리 배운 다음 동적 쿼리로 refactoring 해보
+     * Specification -> JPA에서 동적 쿼리를 작성할 수 있도록 지원하는 인터페이스
+     * 동적쿼리 배운 다음 동적 쿼리로 refactoring 해보
      */
     @Transactional(readOnly = true)
-    public Page<Item> searchItems(String itemName, BigDecimal minPrice, BigDecimal maxPrice, String category, int pageNumber, int pageSize) {
-        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("price").ascending());
+    public Page<Item> searchItems(String itemName, int minPrice, int maxPrice, String categoryName, int pageNumber, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("itemPrice").ascending());
 
         if (itemName != null) {
             return itemRepository.findByItemNameContaining(itemName, pageable);
-        } else if (minPrice != null && maxPrice != null) {
+        } else if (minPrice >= 0 && maxPrice >= 0) {
             return itemRepository.findByItemPriceBetween(minPrice, maxPrice, pageable);
-        } else if (category != null) {
+        } else if (categoryName != null) {
+            Category category = categoryRepository.findByName(categoryName)
+                    .orElseThrow(() -> new IllegalArgumentException("Category not found: " + categoryName));
             return itemRepository.findByCategory(category, pageable);
         }
 
@@ -67,24 +82,24 @@ public class ItemService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ItemDto> findByItemPrice(int price, int pageNumber){
+    public Page<ItemDto> findByItemPrice(int price, int pageNumber) {
         if (pageNumber < 0) {
             log.warn("음수 페이지 번호가 입력되었습니다. 첫 번째 페이지를 반환합니다.");
             pageNumber = 0;
         }
 
-        Pageable pageable = PageRequest.of(pageNumber, 5, Sort.by("price").ascending());
+        Pageable pageable = PageRequest.of(pageNumber, 5, Sort.by("itemPrice").ascending());
         Page<Item> itemPrice = itemRepository.findByItemPrice(price, pageable);
         return itemPrice.map(converter::convertToItemDto);
     }
 
     @Transactional(readOnly = true)
-    public Page<ItemDto> findByItemPriceRange(BigDecimal minPrice, BigDecimal maxPrice, int pageNumber) {
+    public Page<ItemDto> findByItemPriceRange(int minPrice, int maxPrice, int pageNumber) {
         if (pageNumber < 0) {
             log.warn("음수 페이지 번호가 입력되었습니다. 첫 번째 페이지를 반환합니다.");
             pageNumber = 0;
         }
-        Pageable pageable = PageRequest.of(pageNumber, 5, Sort.by("price").ascending());
+        Pageable pageable = PageRequest.of(pageNumber, 5, Sort.by("itemPrice").ascending());
         Page<Item> items = itemRepository.findByItemPriceBetween(minPrice, maxPrice, pageable);
         return items.map(converter::convertToItemDto);
     }
