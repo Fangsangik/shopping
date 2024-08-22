45f3d25eda1f68da3272ec67b2c4c184a068dafc
hwang <hwangsangik@gmail.com>
2024-08-22 20:06:15 +0900

    feat = "category Domain 작성"

---
+++ src/main/java/searching_program/search_product/domain/Category.java
@@ -0,0 +1,34 @@
+package searching_program.search_product.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Item> items = new ArrayList<>();
+
+    public void addItem(Item item) {
+        items.add(item);
+        item.setCategory(this);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+        item.setCategory(null);
+    }
+}
