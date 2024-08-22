80a42a3e5ec4ee82ab615fbcaa59c6eaa18ede88
hwang <hwangsangik@gmail.com>
2024-08-22 20:11:54 +0900

    refactor = "List<Item> findItemsByMemberId(Long memberId); -> List<ItemFavorite> findByMemberUserId(@Param("userId") String userId);로 변경"

--- src/main/java/searching_program/search_product/repository/ItemFavoriteRepository.java
+++ src/main/java/searching_program/search_product/repository/ItemFavoriteRepository.java
@@ -1,13 +1,14 @@
 package searching_program.search_product.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import searching_program.search_product.domain.Item;
 import searching_program.search_product.domain.ItemFavorite;
 
 import java.util.List;
 
 public interface ItemFavoriteRepository extends JpaRepository<ItemFavorite, Long> {
-    void deleteByMemberIdAndItemId(Long memberId, Long itemId);
-
-    List<Item> findItemsByMemberId(Long memberId);
+    void deleteByMemberUserIdAndItemId(String userId, Long itemId);
+    List<ItemFavorite> findByMemberUserId(@Param("userId") String userId);
 }
