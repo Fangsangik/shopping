9c0552afc5b30fc996d67a4c1b442741ca4ae5d3
hwang <hwangsangik@gmail.com>
2024-08-22 20:06:29 +0900

    feat = "category DTO 작성"

---
+++ src/main/java/searching_program/search_product/dto/CategoryDto.java
@@ -0,0 +1,18 @@
+package searching_program.search_product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+    private Long id;
+    private String name;
+}
+
