59a87f10d9671abaadb6950a5abbcf299f20fcf1
SangikHwang <hwangsangik@gmail.com>
2024-08-28 23:56:22 +0900

    feat = "인증 범위 설정"

--- src/main/java/searching_program/search_product/config/SecurityConfig.java
+++ src/main/java/searching_program/search_product/config/SecurityConfig.java
@@ -38,24 +38,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.csrf().disable() -> 개발 환경에서만 disable
+                .csrf().disable() // 개발 환경에서만 비활성화
                 .authorizeRequests()
-                .antMatchers("/register", "/login").permitAll()
-                // 나중에 ADMIN 역할을 가진 사용자만 접근 가능하도록 변경
-                .antMatchers("/delete/**").permitAll()
+                // 로그인과 회원가입, 로그아웃은 누구나 접근 가능
+                .antMatchers("/api/auth/login", "/api/register", "/api/auth/logout").permitAll()
+                // 로그인한 사용자라면 접근 가능 (ROLE_USER 또는 ROLE_ADMIN) -> 개발 모드에서는 permit으로 임시 설정
+                .antMatchers("/members/**", "/bucket/**", "/category/**", "/favorite/**",
+                        "/items/**", "/notification/**", "/orders/**", "/payment/**",
+                        "/promotion/**", "/review/**").permitAll()
+                // 특정 페이지는 로그인한 사용자 모두 접근 가능
+                .antMatchers("/login", "/login/register").permitAll()
+                // 관리자만 접근 가능
+                .antMatchers("/delete/**").hasRole("ADMIN")
+                // 그 외 모든 요청은 인증된 사용자만 접근 가능
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
-                .usernameParameter("userId") // 이 부분이 폼에서 전달받는 필드명과 일치해야 함
-                .passwordParameter("password")
-                .defaultSuccessUrl("/members", true)
-                .failureUrl("/login?error")
-                .permitAll()
+                .loginPage("/login")  // 커스텀 로그인 페이지 경로
+                .usernameParameter("userId") // 폼에서 사용자 아이디 필드 이름
+                .passwordParameter("password") // 폼에서 비밀번호 필드 이름
+                .defaultSuccessUrl("/members", true) // 로그인 성공 후 이동할 기본 URL
+                .failureUrl("/login?error") // 로그인 실패 시 이동할 URL
+                .permitAll() // 로그인 폼은 모두 접근 가능
                 .and()
                 .logout()
-                .permitAll();
+                .logoutUrl("/api/logout") // 로그아웃 URL
+                .permitAll(); // 로그아웃도 모두 접근 가능
     }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
