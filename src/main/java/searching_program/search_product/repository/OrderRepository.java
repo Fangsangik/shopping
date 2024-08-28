8599ba3873482af7686688dccf7b50da01a0ad1d
SangikHwang <hwangsangik@gmail.com>
2024-08-28 23:49:26 +0900

    feat = "findByItem_ItemNameContaining"

--- src/main/java/searching_program/search_product/repository/OrderRepository.java
+++ src/main/java/searching_program/search_product/repository/OrderRepository.java
@@ -14,4 +14,7 @@ public interface OrderRepository extends JpaRepository<Orders, Long> {
     Page<Orders> findByItemItemName(String itemDto, Pageable pageable);
 
     Page<Orders> findByMemberUserId (String userId, Pageable pageable);
+
+    Page<Orders> findByItem_ItemNameContaining(String itemName, Pageable pageable);
+
 }
